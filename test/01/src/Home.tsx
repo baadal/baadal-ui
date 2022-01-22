4a19cffc5b3b521a415f293a8857a05ff2828208
Abhishek Raj <abhi@raj.me>
2022-01-22 16:40:05 +0530

    x1

--- test/01/src/Home.tsx
+++ test/01/src/Home.tsx
@@ -7,7 +7,11 @@ function App() {
     <div className="app">
       <header className="appHeader">
         <h3 className="appTitle">List of Components</h3>
-        <Link to="/button-demo" className="appLink">Button Demo</Link>
+        <div>
+          <span>Button:</span>&nbsp;&nbsp;
+          <Link to="/button-demo" className="appLink">Demo</Link>&nbsp;&nbsp;
+          <Link to="/button-preview" className="appLink">Preview</Link>
+        </div>
         <br /><br />
         <a className="externalLink" href="https://github.com/baadal/baadal-ui">GitHub repo</a>
         <a className="externalLink" href="https://www.npmjs.com/package/@baadal-sdk/baadal-ui">NPM package</a>
